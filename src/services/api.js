663fc894532c582d722e0ddf1edf3c3742d48e9c
kayogeraldi <kayo.geraldi@gmail.com>
2025-02-01 09:57:42 -0300

    ajsute pra funcionar

--- src/services/api.js
+++ src/services/api.js
@@ -3,10 +3,11 @@
 import axios from  'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.16.112:3333'
+    baseURL: 'http://localhost:3333'
 });
 
 //192.168.16.117
+
 
 //192.168.16.106 casa
 export default api;
